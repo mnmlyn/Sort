10e4d36b2d337ada723b64102338d7cf835aef53
mnmlyn <mnmlyn@163.com>
2018-09-07 18:31:47 +0800

    头文件

---
+++ include/Sort-main.h
@@ -0,0 +1,25 @@
+#ifndef _SORT_MAIN_H_
+#define _SORT_MAIN_H_
+#include <iostream>
+
+//冒泡排序
+void BubbleSort(int *arr,int n);
+//冒泡排序-优化-无交换直接退出
+void BubbleSort_noexchangeexit(int *arr,int n);
+
+//选择排序
+void SelectSort(int *arr,int n);
+
+//插入排序
+void InsertSort(int *arr,int n);
+
+//快速排序
+void QuickSort(int *arr,int n);
+
+//堆排序
+void HeapSort(int *arr,int n);
+
+//归并排序
+void MergeSort(int *arr,int n);
+
+#endif
